f5bb22ba27b8045f773824e29e65f8c750b0ba05
georges sysaath <georges.sysaath@gmail.com>
2020-11-13 12:19:59 +0100

    before all rework

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+Ingredient.destroy_all
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+parse = JSON.parse(open(url).read)
+parse["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Sex on the Beach")
+Cocktail.create(name: "Margarita")
+Cocktail.create(name: "Pinacolada")
+
+Dose.create(description: "2 cl", cocktail_id: 1, ingredient_id: 1)
+Dose.create(description: "2 cl", cocktail_id: 1, ingredient_id: 2)
+Dose.create(description: "2 cl", cocktail_id: 1, ingredient_id: 3)
+Dose.create(description: "2 cl", cocktail_id: 1, ingredient_id: 4)
+Dose.create(description: "2 cl", cocktail_id: 1, ingredient_id: 5)
+Dose.create(description: "2 cl", cocktail_id: 1, ingredient_id: 6)
+Dose.create(description: "2 cl", cocktail_id: 2, ingredient_id: 7)
+Dose.create(description: "2 cl", cocktail_id: 2, ingredient_id: 8)
+Dose.create(description: "2 cl", cocktail_id: 2, ingredient_id: 9)
+Dose.create(description: "2 cl", cocktail_id: 2, ingredient_id: 10)
+Dose.create(description: "2 cl", cocktail_id: 2, ingredient_id: 11)
+Dose.create(description: "2 cl", cocktail_id: 2, ingredient_id: 12)
+Dose.create(description: "2 cl", cocktail_id: 2, ingredient_id: 13)
+Dose.create(description: "2 cl", cocktail_id: 2, ingredient_id: 14)
+
